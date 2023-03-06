e9e4ce88ee9e11ab031b0c0db72454836b5f7ec3
BinkaArushankina <albinarushankina@gmail.com>
2023-03-06 10:24:24 +0100

    with while loop

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException{
+        File filename = new File("textFile.txt");
+        System.out.println(returnString(filename));  // aaa bbbb cc
 
-        System.out.println(returnString("textFile.txt"));  // aaa bbbb cc
-
-        System.out.println(arithmeticMean("notEmptyTextFile.txt"));  // 3
+        System.out.println(countAverage("notEmptyTextFile.txt"));  // 3
 
     }
     //Homework Задача 1
@@ -18,13 +19,17 @@ public class Main {
     // bbbb
     // cc
     //Метод вернет aaa bbbb cc
-    public static String returnString(String fileText)throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(fileText));
-        String line1= br.readLine();
-        String line2= br.readLine();
-        String line3= br.readLine();
-
-        return line1+" "+line2+" "+line3;
+    public static String returnString(File fileText) {
+        String s ="";
+        try(BufferedReader br = new BufferedReader(new FileReader(fileText))) {
+            String line;
+            while ((line= br.readLine())!=null){
+                s=s+" "+line;
+            }
+        }catch (IOException e){
+            e.getMessage();
+        }
+        return s;
     }
     //Задача 2
     //Есть не пустой текстовый файл такого вида:
@@ -34,15 +39,20 @@ public class Main {
     //4
     //5
     //Написать метод, возвращаюший среднее арифметическое всех этих чисел.
-    public static int arithmeticMean(String textFile)throws IOException{
-        BufferedReader br= new BufferedReader(new FileReader(textFile));
-        int one = Integer.parseInt(br.readLine());
-        int two = Integer.parseInt(br.readLine());
-        int three = Integer.parseInt(br.readLine());
-        int four = Integer.parseInt(br.readLine());
-        int five = Integer.parseInt(br.readLine());
-        int sum = one+two+three+four+five;
+    public static int countAverage(String textFile)throws IOException{
 
-        return sum/5;
+        int sum=0;
+        int count=0;
+        String nextInt;
+        try (BufferedReader br= new BufferedReader(new FileReader(textFile))){
+            while ((nextInt= br.readLine())!=null){
+                sum=sum+Integer.parseInt(nextInt);//poschitali summu
+                count++;                          //poschitali kolwo strok
+            }
+        }catch(IOException e){
+            e.getMessage();
+        }
+
+        return sum/count;
     }
 }
